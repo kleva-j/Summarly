3e853a504842248bd3eee6d2c8061af244c32417
kleva-j <kasmickleva@gmail.com>
2025-02-04 23:11:00 +0100

    feat: setup shadcn components

---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Summarly",
+  title: "Summarly",
+  description: "When summary meets editing.",
+  navItems: [],
+  mainNav: [],
+  links: {
+    twitter: "https://twitter.com/vercel",
+    github: "https://github.com/vercel/next.js",
+  },
+};
