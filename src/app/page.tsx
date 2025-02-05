556d29c8abcb74c4dc9af181edeab14ae47828b9
kleva-j <kasmickleva@gmail.com>
2025-02-06 00:23:30 +0100

    feat: add a faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/layout/header";
 import { Hero } from "@/layout/hero";
+import { FAQ } from "@/layout/faq";
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
 			<Hero />
+			<FAQ />
 		</div>
 	);
 }
