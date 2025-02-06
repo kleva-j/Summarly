005465b7c3e3467e659e7380d92a89193c3b1d36
kleva-j <kasmickleva@gmail.com>
2025-02-06 18:51:37 +0100

    feat: install and setup clerk auth with env variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,26 @@ import type { PropsWithChildren } from "react";
 import type { Metadata } from "next";
 
 import { geistMono, geistSans } from "@/lib/font";
+import { ClerkProvider } from "@clerk/nextjs";
 import { siteConfig } from "@/lib/config";
 
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
-  title: siteConfig.title,
-  description: siteConfig.description,
+	title: siteConfig.title,
+	description: siteConfig.description,
 };
 
 export default function RootLayout(props: PropsWithChildren) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {props.children}
-      </body>
-    </html>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					{props.children}
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
