f67ad9e7348a7b0a085bb3d6962291b06cceb3ac
kleva-j <kasmickleva@gmail.com>
2025-02-07 13:32:49 +0100

    chore: setup env-nextjs, convex-helper and zod

---
+++ env.mjs
@@ -0,0 +1,32 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    CONVEX_DEPLOYMENT: z.string().min(10),
+    CLERK_SECRET_KEY: z.string().min(10),
+    CLERK_ISSUER_URL: z.string().url("Must be a valid URL"),
+  },
+  client: {
+    NEXT_PUBLIC_CONVEX_URL: z.string().min(10),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(10),
+  },
+  runtimeEnv: {
+    CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_ISSUER_URL: process.env.CLERK_ISSUER_URL,
+  },
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
+});
