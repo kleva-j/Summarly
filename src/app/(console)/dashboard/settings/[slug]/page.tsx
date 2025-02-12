e4dbc12b93e6f8e6202e27be881966ceabeedee5
kleva-j <kasmickleva@gmail.com>
2025-02-12 15:50:17 +0100

    feat: implement settings layout, page and dynamic nested routes

---
+++ src/app/(console)/dashboard/settings/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import dynamic from "next/dynamic";
+
+const components = {
+	appearance: dynamic(async () => (await import("./_appearance")).Appearance),
+	advanced: dynamic(async () => (await import("./_advanced")).Advanced),
+};
+
+interface SettingsPageProps {
+	params: Awaited<{
+		slug: keyof typeof components;
+	}>;
+}
+
+export default async function SettingsPage({ params }: SettingsPageProps) {
+	const { slug } = await params;
+	
+  const Component = components[slug];
+
+	return (
+		<div className="flex flex-1 flex-col gap-4 overflow-y-auto p-4 pt-0">
+			<Component />
+		</div>
+	);
+}
