59083862051a90a1755be08bacc0f0ff317c95db
kleva-j <kasmickleva@gmail.com>
2025-02-20 12:15:06 +0100

    feat: secure dashboard page/route

--- src/lib/posthog/node-client.ts
+++ src/lib/posthog/node-client.ts
@@ -5,5 +5,11 @@ const host = env.NEXT_PUBLIC_POSTHOG_HOST;
 const key = env.NEXT_PUBLIC_POSTHOG_KEY;
 
 export function PostHogNodeClient() {
-  return new PostHog(key, { host });
+  /**
+   * flushAt - sets how many capture calls we should flush the queue (in one batch).
+   * flushInterval - sets how many milliseconds we should wait before flushing the queue.
+   * Setting them to the lowest number ensures events are sent immediately and not batched.
+   * We also need to call await posthog.shutdown() once done.
+   */
+  return new PostHog(key, { host, flushAt: 1, flushInterval: 0 });
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/dashboard(.*)",
-]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+const isPrivateRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
-  }
+  if (!isPublicRoute(request)) await auth.protect();
 });
 
-export const config = {
+const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
@@ -21,3 +15,5 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+export { config, isPublicRoute, isPrivateRoute };
