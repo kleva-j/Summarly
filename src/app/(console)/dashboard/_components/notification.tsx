7eaa823b4c8d2a3c339d67ae583a64101eebf20b
kleva-j <kasmickleva@gmail.com>
2025-03-01 10:37:54 +0100

    fix: minor changes

--- src/app/(console)/dashboard/_components/notification.tsx
+++ src/app/(console)/dashboard/_components/notification.tsx
@@ -11,11 +11,15 @@ import {
 
 type NotificationProps = SharedProps & { notifications: Doc<"notifs">[] };
 
-export const Notifications = ({ title, notifications }: NotificationProps) => {
-	return (
+const textHeading = "Feels lonely here";
+
+export const Notifications = ({ notifications }: NotificationProps) => {
+	return notifications.length > 0 ? (
+		<div>Notifications</div>
+	) : (
 		<Card className="col-span-4 lg:col-span-3">
 			<CardHeader>
-				<CardTitle>Feels lonely here</CardTitle>
+				<CardTitle>{textHeading}</CardTitle>
 				<CardDescription>
 					You have no notifications. If you had notifications, there would be a{" "}
 					<Bell className="h-4 w-4 inline-block" /> icon right next to the
