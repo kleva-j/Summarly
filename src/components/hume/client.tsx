846535dd3883ce63e374d9d1ecd6771d93619995
kleva-j <kasmickleva@gmail.com>
2025-04-14 16:06:18 +0100

    feat: add logo, handle message event and show message on failed connection

--- src/components/hume/client.tsx
+++ src/components/hume/client.tsx
@@ -1,15 +1,39 @@
 "use client";
 
-import type { PropsWithChildren } from "react";
+import type { ComponentRef } from "react";
 
+import { StartCall } from "@/components/hume/start-call";
+import { Messages } from "@/components/hume/message";
+import { Controls } from "@/components/hume/control";
 import { VoiceProvider } from "@humeai/voice-react";
+import { useRef } from "react";
 
-type HumeClientProps = PropsWithChildren<{ accessToken: string }>;
+type HumeClientProps = { accessToken: string };
 
-export function HumeClient({ accessToken, children }: HumeClientProps) {
-	return (
-		<VoiceProvider auth={{ type: "accessToken", value: accessToken }}>
-			{children}
-		</VoiceProvider>
-	);
+export function HumeClient({ accessToken }: HumeClientProps) {
+  const timeout = useRef<number | null>(null);
+  const ref = useRef<ComponentRef<typeof Messages> | null>(null);
+
+  return (
+    <div className="relative grow flex flex-col mx-auto w-full overflow-hidden h-[0px]">
+      <VoiceProvider
+        auth={{ type: "accessToken", value: accessToken }}
+        onMessage={() => {
+          if (timeout.current) window.clearTimeout(timeout.current);
+
+          timeout.current = window.setTimeout(() => {
+            if (ref.current) {
+              const scrollHeight = ref.current.scrollHeight;
+
+              ref.current.scrollTo({ top: scrollHeight, behavior: "smooth" });
+            }
+          }, 200);
+        }}
+      >
+        <Messages ref={ref} />
+        <Controls />
+        <StartCall />
+      </VoiceProvider>
+    </div>
+  );
 }
